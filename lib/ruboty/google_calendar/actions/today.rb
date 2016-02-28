06e8f863f61b6743e92f780ec707df26036eba5e
gomao9 <gomaoq@gmail.com>
2016-02-28 23:54:32 +0900

    enable date specification for 'ruboty 今日の予定'

--- lib/ruboty/google_calendar/actions/today.rb
+++ lib/ruboty/google_calendar/actions/today.rb
@@ -6,15 +6,24 @@ module Ruboty
         def call
           message.reply(today)
         rescue => e
-          message.reply(e.message)
+          message.reply(e.message + "\n" + e.backtrace.join("\n"))
         end
 
         private
 
         def today
+          day = if message[:date]
+                  year, month, day = message[:date].scan(/(\d{4})(\d{2})(\d{2})/).first
+                  Time.new(year, month, day)
+                else
+                  Time.now
+                end
+
           ids = ENV['GOOGLE_CALENDAR_IDS'].split(',')
           client = Ruboty::GoogleCalendar::CalendarClient.new ENV
-          ids.flat_map{|id| client.search(id)}.map(&:summary).join("\n")
+          ids.flat_map{|id| client.search(id, day)}.uniq(&:id).map do |event|
+            event.summary
+          end.join("\n")
         end
 
       end
--- lib/ruboty/handlers/google_calendar.rb
+++ lib/ruboty/handlers/google_calendar.rb
@@ -5,7 +5,7 @@ module Ruboty
   module Handlers
     # Replies events in GoogleCalendar
     class GoogleCalendar < Base
-      on /今日の予定\z/, name: 'today', description: "Replies today's events"
+      on /今日の予定?(?<date> \d{8})\z/, name: 'today', description: "Replies today's events"
       env :GOOGLE_CALENDAR_CLIENT_ID, "client id"
       env :GOOGLE_CALENDAR_CLIENT_SECRET, "client secret"
       env :GOOGLE_CALENDAR_SCOPE, "scope"
