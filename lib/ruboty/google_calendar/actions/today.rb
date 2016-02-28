6bdc6b7d25a9d26811aa3fbb58f0a0a31d98fbaa
gomao9 <gomaoq@gmail.com>
2016-02-29 00:30:37 +0900

    display formatted event

--- lib/ruboty/google_calendar/actions/today.rb
+++ lib/ruboty/google_calendar/actions/today.rb
@@ -1,32 +1,41 @@
 require 'ruboty'
-module Ruboty
-  module GoogleCalendar
-    module Actions
-      class Today < Ruboty::Actions::Base
-        def call
-          message.reply(today)
-        rescue => e
-          message.reply(e.message + "\n" + e.backtrace.join("\n"))
-        end
-
-        private
-
-        def today
-          day = if message[:date]
-                  year, month, day = message[:date].scan(/(\d{4})(\d{2})(\d{2})/).first
-                  Time.new(year, month, day)
-                else
-                  Time.now
-                end
-
-          ids = ENV['GOOGLE_CALENDAR_IDS'].split(',')
-          client = Ruboty::GoogleCalendar::CalendarClient.new ENV
-          ids.flat_map{|id| client.search(id, day)}.uniq(&:id).map do |event|
-            event.summary
-          end.join("\n")
-        end
-
-      end
+module Ruboty::GoogleCalendar::Actions
+  class Today < Ruboty::Actions::Base
+    HHMM_FORMAT = "%H:%M"
+    ALLDAY      = "終日 "
+    def call
+      message.reply(today)
+    rescue => e
+      message.reply(e.message + "\n" + e.backtrace.join("\n"))
     end
+
+    private
+
+    def today
+      day = if message[:date]
+              year, month, day = message[:date].scan(/(\d{4})(\d{2})(\d{2})/).first
+              Time.new(year, month, day)
+            else
+              Time.now
+            end
+
+      ids = ENV['GOOGLE_CALENDAR_IDS'].split(',')
+      client = Ruboty::GoogleCalendar::CalendarClient.new ENV
+      ids.flat_map{|id| client.search(id, day)}.uniq(&:id).map do |event|
+        time = if event.start.date_time
+                 time_format(event.start.date_time, event.end.date_time)
+               else
+                 ALLDAY
+               end
+        atendees = event.attendees.map(&:display_name).join(',')
+
+        sprintf("%s[%s]:%s", time, atendees, event.summary)
+      end.join("\n")
+    end
+
+    def time_format st, ed
+      "#{st.strftime(HHMM_FORMAT)}〜#{ed.strftime(HHMM_FORMAT)}"
+    end
+
   end
 end
