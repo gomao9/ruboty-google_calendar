25be4e70171d9ab73a271a2340575cea874afdef
Sugita Haruhi <sugita@wyrd.co.jp>
2016-02-29 17:14:22 +0900

    update event format

--- lib/ruboty/google_calendar/actions/today.rb
+++ lib/ruboty/google_calendar/actions/today.rb
@@ -1,8 +1,9 @@
+require 'json'
 require 'ruboty'
 module Ruboty::GoogleCalendar::Actions
   class Today < Ruboty::Actions::Base
     HHMM_FORMAT = "%H:%M"
-    ALLDAY      = "終日 "
+    ALLDAY      = "終日"
     def call
       message.reply(today)
     rescue => e
@@ -12,22 +13,25 @@ module Ruboty::GoogleCalendar::Actions
     private
 
     def today
-      day = if message[:date]
+      day = if message[:date].present?
               year, month, day = message[:date].scan(/(\d{4})(\d{2})(\d{2})/).first
               Time.new(year, month, day)
             else
               Time.now
             end
 
-      ids = ENV['GOOGLE_CALENDAR_IDS'].split(',')
+      ids = JSON.parse(ENV['GOOGLE_CALENDAR_IDS'])
       client = Ruboty::GoogleCalendar::CalendarClient.new ENV
-      ids.flat_map{|id| client.search(id, day)}.uniq(&:id).map do |event|
+      events = ids.keys.flat_map{|id| client.search(id, day).map{|event| [id, event]}}
+
+      events.sort_by{|_, event| event.start.date_time.to_i}.group_by{|_, event| event.id}.map do |id, events|
+        event = events.first[1]
         time = if event.start.date_time
                  time_format(event.start.date_time, event.end.date_time)
                else
                  ALLDAY
                end
-        atendees = event.attendees.map(&:display_name).join(',')
+        atendees = events.map{|id, _| ids[id]}.join(',')
 
         sprintf("%s[%s]:%s", time, atendees, event.summary)
       end.join("\n")
